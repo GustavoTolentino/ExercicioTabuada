6bacb00ef434654ea342351dd50eeaf0159f9f71
TolentinoGustavo <tolentinogustavo@gmail.com>
2020-11-24 16:36:58 -0300

    Projeto Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioTabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem Vindo a Tabuada C#");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("----------------------");
+                for (int j = 0; j < 11; j++)
+                {
+                    
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+                Console.WriteLine("----------------------");
+            }
+        }
+    }
+}
